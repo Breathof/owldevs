c32a1180b188ca8b661de4d2bbb6bff9a9c3ad51
Breathof <nachofcz@gmail.com>
2018-09-29 18:21:05 -0300

    Page, If not found shown review github host configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'owldevs';
+export class AppComponent implements OnInit {
+  title = 'Owl Devs';
+  public width: number = 200;
+  public touch = false;
+  
+  constructor() {
+    this.width = 200;
+  }
+
+  ngOnInit(): void {
+    console.log(document.getElementById('logo'));
+    this.width = document.getElementById('logo').clientWidth;
+  }
+
+  public move(event) {
+    if (this.touch) {
+      this.width += 50;
+      this.touch = !this.touch;
+    } else {
+      this.width -= 50;
+      this.touch = !this.touch;
+    }
+    console.log("Event fired!");
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  // baseRef: "breathof.github.io/owldevs/"
 };
