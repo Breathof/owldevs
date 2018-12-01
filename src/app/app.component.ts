a6c728d8ae81290516b19008f81f8cff19b48466
Clovis <nachofcz@gmail.com>
2018-12-01 00:30:08 -0300

    sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'Owl Devs';
   public width: number = 200;
   public touch = false;
   
@@ -15,18 +14,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log(document.getElementById('logo'));
     this.width = document.getElementById('logo').clientWidth;
   }
 
-  public move(event) {
-    if (this.touch) {
-      this.width += 50;
-      this.touch = !this.touch;
-    } else {
-      this.width -= 50;
-      this.touch = !this.touch;
-    }
-    console.log("Event fired!");
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+
+const routes: Routes = [
+  { path:'', component: HomeComponent},
+  { path:'projects', component: ProjectsComponent},
+  { path:'about-us', component: AboutUsComponent},
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProjectsComponent,
+    AboutUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      routes,
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
